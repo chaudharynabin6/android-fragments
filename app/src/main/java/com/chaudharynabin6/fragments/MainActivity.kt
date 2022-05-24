d0d8733130dcbeea44e36c0883cee0d7d7257da8
chaudharynabin6 <chaudharynabin6@gmail.com>
2022-05-24 15:22:42 +0545

    initialising fragments from activity programmatically

--- app/src/main/java/com/chaudharynabin6/fragments/MainActivity.kt
+++ app/src/main/java/com/chaudharynabin6/fragments/MainActivity.kt
@@ -2,10 +2,19 @@ package com.chaudharynabin6.fragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import com.chaudharynabin6.fragments.fragment_basics.FragmentBasics
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<FragmentBasics>(R.id.fragment_container_view)
+            }
+        }
         setContentView(R.layout.activity_main)
     }
 }
